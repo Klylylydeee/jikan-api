553073079c643daaf23bfbf3b37e637183306156
klylylydeee <klylylydeee@gmail.com>
2021-10-04 20:41:08 +0800

    add documentation

--- Route/route.js
+++ Route/route.js
@@ -22,4 +22,34 @@ router.patch('/patch/:title', animeCollectionController.patchAnimeByTitle);
 
 router.post('/add/id/:id', animeCollectionController.postAnimeDocument);
 
+router.get("/admin", async (req,res,next) => {
+    res.status(200).send({
+      admin: "Welcome to AniDB API EndPoint List",
+      get: {
+        jikan: {
+          fetch_based_on_titles: `${process.env.HOME_URL}/jikan-api/title/:title`,
+          fetch_based_on_id: `${process.env.HOME_URL}/jikan-api/id/:id`
+        },
+        ani_db: {
+          get_anime_complete: `${process.env.HOME_URL}/anime/only`,
+          get_anime_count_by_letter: `${process.env.HOME_URL}/anime/only`,
+          get_anime_by_genre: `${process.env.HOME_URL}/anime/:genre`,
+          get_anime_by_starting_and_ending_date: `${process.env.HOME_URL}/dl/date?start=StartYear-StartMonth-StartDay&end=EndYear-EndMonth-EndDay`,
+          get_anime_by_starting_letter: `${process.env.HOME_URL}/letter/:letter`,
+          get_anime_by_stitle: `${process.env.HOME_URL}/title/:title`
+        }
+      },
+      post: {
+        add_anime_based_from_jikan_api_id: `${process.env.HOME_URL}/add/id/:id`,
+        add_custom_whole_data: `${process.env.HOME_URL}/anime/custom`
+      },
+      patch: {
+        update_an_existing_anime_document: `${process.env.HOME_URL}/patch/:title`
+      },
+      delete: {
+        delete_an_existing_anime_document: `${process.env.HOME_URL}/delete/:title`
+      }
+    })
+  });
+
 module.exports = router;
--- server.js
+++ server.js
@@ -20,6 +20,26 @@ app.listen(process.env.PORT || 5000, () =>
   console.log(`Running at port http://localhost:${process.env.PORT || 5000}/`)
 );
 
+app.get("/", async (req,res,next) => {
+  res.status(200).send({
+    admin: "Welcome to AniDB API EndPoint List",
+    get: {
+      jikan: {
+        fetch_based_on_titles: `${process.env.HOME_URL}/jikan-api/title/:title`,
+        fetch_based_on_id: `${process.env.HOME_URL}/jikan-api/id/:id`
+      },
+      ani_db: {
+        get_anime_complete: `${process.env.HOME_URL}/anime/only`,
+        get_anime_count_by_letter: `${process.env.HOME_URL}/anime/only`,
+        get_anime_by_genre: `${process.env.HOME_URL}/anime/:genre`,
+        get_anime_by_starting_and_ending_date: `${process.env.HOME_URL}/dl/date?start=StartYear-StartMonth-StartDay&end=EndYear-EndMonth-EndDay`,
+        get_anime_by_starting_letter: `${process.env.HOME_URL}/letter/:letter`,
+        get_anime_by_stitle: `${process.env.HOME_URL}/title/:title`,
+      }
+    }
+  })
+});
+
 app.use('/', require('./Route/route'));
 app.use('/jikan-api', require('./Route/path.jikan'));
 app.use(errorController.get404);
